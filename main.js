00868a48d695feaa1855ba060c17d56f19652ef9
murilpcarneiro <murilpcarneiro@gmail.com>
2024-11-26 00:00:50 -0300

    commit - falta colocar alguma nova funcionalidade-

--- main.js
+++ main.js
@@ -2,14 +2,21 @@ document.addEventListener('DOMContentLoaded', () => {
   const min = document.getElementById('min');
   const max = document.getElementById('max');
   const btn = document.getElementById('btn');
+  const quantidade = document.getElementById('quantidade');
   const resultado = document.getElementById('resultado');
 
   btn.addEventListener('click', () => {
-    console.log('click');
     const minimo = parseInt(min.value);
     const maximo = parseInt(max.value);
-    const aleatorio = Math.floor(Math.random() * (maximo - minimo + 1) + minimo);
-    resultado.innerHTML = aleatorio;
+    const qtd = parseInt(quantidade.value);
+    const numeros = [];
+
+    for (let i = 0; i < qtd; i++) {
+      const numero = Math.floor(Math.random() * (maximo - minimo + 1)) + minimo;
+      numeros.push(numero);
+    }
+
+    resultado.textContent = numeros.join(', ');
   });
 });
 
